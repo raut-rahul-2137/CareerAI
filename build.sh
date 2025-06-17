289625d6fd2e8fc3b03fd4f8ccd4e38b202102da
raut-rahul-2137 <142197737+raut-rahul-2137@users.noreply.github.com>
2025-06-17 17:45:19 +0530

    update

--- build.sh
+++ build.sh
@@ -5,17 +5,8 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Create necessary directories
-mkdir -p staticfiles
-mkdir -p media
-
-# Set proper permissions
-chmod -R 755 static
-chmod -R 755 staticfiles
-chmod -R 755 media
-
 # Collect static files
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate 
--- career_ai/settings.py
+++ career_ai/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -144,11 +143,3 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Configure WhiteNoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Additional static files settings
-WHITENOISE_USE_FINDERS = True
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_ALLOW_ALL_ORIGINS = True
